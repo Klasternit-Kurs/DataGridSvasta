78860b0c47f2b7ed0d5795d1996c942a97d73e56
Dragan Pepic <d.pepic@hotmail.com>
2020-04-16 17:57:18 +0200

    dsdf

--- DataGridSvasta/MainWindow.xaml.cs
+++ DataGridSvasta/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,36 +21,107 @@ namespace DataGridSvasta
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+	/// 
+
+	public class PromenaCeneArgs
+	{
+		public decimal cena;
+
+		public PromenaCeneArgs(decimal c) => cena = c;
+		
+	}
 	public partial class MainWindow : Window
 	{
 		ObservableCollection<Artikal> lstArt = new ObservableCollection<Artikal>();
+		public decimal UnosCena { get; set; }
+
+		public delegate void PromenaCeneHandler(object posiljaoc, PromenaCeneArgs a);
+		public event PromenaCeneHandler OnPromenaCene;
+
+		public List<Artikal> lArt = new List<Artikal>();
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
+			DataContext = this;
+
 			lstArt.Add(new Artikal("Nesto", 20));
 			lstArt.Add(new Artikal("Nesto drugo", 30));
 			lstArt.Add(new Artikal("Nesto trece", 70));
+			lstArt.Add(new Artikal("Plazam", 200));
+			lstArt.Add(new Artikal("Jos nesto", 770));
+			lstArt.Add(new Artikal("Bla", 40));
+			lstArt.Add(new Artikal("Bla bla", 890));
 
 			dg.ItemsSource = lstArt;
 		}
 
-		private void Cekirano(object sender, RoutedEventArgs e)
+		private void Obrisi(object sender, RoutedEventArgs e)
 		{
 			//ToggleButton tb = sender as ToggleButton;
 			//Artikal a = tb.DataContext as Artikal;
 
 			//Artikal a = (sender as ToggleButton).DataContext as Artikal;
 
-			lstArt.Remove((sender as ToggleButton).DataContext as Artikal);
+			lstArt.Remove((sender as Button).DataContext as Artikal);
 		}
+
+		private void Promeni(object sender, RoutedEventArgs e)
+		{
+			OnPromenaCene?.Invoke(this, new PromenaCeneArgs(UnosCena));
+
+			//foreach (Artikal a in lArt)
+			//{
+			//	a.Cena = UnosCena;
+			//}
+
+			//lArt.ForEach(a => a.Cena = UnosCena);
+
+		}
+
+		private void Cekiran(object sender, RoutedEventArgs e)
+		{
+			OnPromenaCene += ((sender as ToggleButton).DataContext as Artikal).Promena;
+			//lArt.Add(((sender as ToggleButton).DataContext as Artikal));
+		}
+
+		private void Odcekiran(object sender, RoutedEventArgs e)
+		{
+			OnPromenaCene -= ((sender as ToggleButton).DataContext as Artikal).Promena;
+			//lArt.Remove(((sender as ToggleButton).DataContext as Artikal));
+		}
+
+		/*private void Kliknut(object sender, RoutedEventArgs e)
+		{
+			ToggleButton tb = sender as ToggleButton;
+
+			if (tb.IsChecked == true)
+			{
+				OnPromenaCene += ((sender as ToggleButton).DataContext as Artikal).Promena;
+			}
+			else 
+			{
+				OnPromenaCene -= ((sender as ToggleButton).DataContext as Artikal).Promena;
+			}
+		}*/
 	}
 
-	public class Artikal
+	public class Artikal : INotifyPropertyChanged
 	{
 		public string Naziv { get; set; }
-		public decimal Cena { get; set; }
+
+		private decimal cena;
+		public decimal Cena 
+		{
+			get => cena; 
+			set
+			{
+				cena = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Cena"));
+			}
+		}
+		
 		public bool Selektovano { get; set; }
 
 		public Artikal(string n, decimal c)
@@ -59,6 +131,13 @@ namespace DataGridSvasta
 		}
 
 		public Artikal() { }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void Promena(object p, PromenaCeneArgs a)
+		{
+			Cena = a.cena;
+		}
 
 	}
 }
